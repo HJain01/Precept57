e82990062e3cb7d8cb9d7599d04a9780dca12d97
HJain01 <jainh9999@gmail.com>
2022-05-29 19:06:26 -0400

    Adding base classes for precepts

---
+++ Precept57/Precept.cs
@@ -0,0 +1,20 @@
+﻿namespace Precept57
+{
+    internal abstract class Precept
+    {
+        public abstract string Sprite { get; }
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+        public abstract string Scene { get; }
+        public abstract float X { get; }
+        public abstract float Y { get; }
+
+        // assigned at runtime by SFCore's CharmHelper
+        public int Num { get; set; }
+
+
+        public abstract PreceptSettings Settings(SaveSettings s);
+
+        public virtual void Hook() {}
+    }
+}
---
+++ Precept57/SaveSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Precept57
+{
+    public class SaveSettings
+    {
+        public PreceptSettings Precept35 = new();
+    }
+}
