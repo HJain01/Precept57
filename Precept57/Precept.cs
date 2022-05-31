c74a0da3862084370b8cd0311d56600526e9f4ce
HJain01 <jainh9999@gmail.com>
2022-05-30 21:39:56 -0400

    Trying to add item into game to be able to be picked up

--- Precept57/Precept.cs
+++ Precept57/Precept.cs
@@ -4,14 +4,11 @@
     {
         public abstract string Sprite { get; }
         public abstract string Name { get; }
+        public abstract string Id { get; }
         public abstract string Description { get; }
         public abstract string Scene { get; }
         public abstract float X { get; }
         public abstract float Y { get; }
-
-        // assigned at runtime by SFCore's CharmHelper
-        public int Num { get; set; }
-
 
         public abstract PreceptSettings Settings(SaveSettings s);
 
--- Precept57/Precept57.cs
+++ Precept57/Precept57.cs
@@ -1,47 +1,113 @@
 ﻿using Modding;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UObject = UnityEngine.Object;
+using ItemChanger;
+using ItemChanger.Items;
+using ItemChanger.Locations;
+using ItemChanger.UIDefs;
+using SFCore;
 
 namespace Precept57
 {
-    public class Precept57 : Mod
+    public class Precept57 : Mod, ILocalSettings<SaveSettings>
     {
         private static readonly List<Precept> Precepts = new()
         {
             Precept35.Instance
         };
         
-        internal static Precept57 Instance;
+        private static Precept57 Instance;
 
-        //public override List<ValueTuple<string, string>> GetPreloadNames()
-        //{
-        //    return new List<ValueTuple<string, string>>
-        //    {
-        //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
-        //    };
-        //}
+        private Dictionary<string, Func<bool, bool>> BoolGetters = new();
+        private Dictionary<string, Action<bool>> BoolSetters = new();
 
         public Precept57() : base("Precept 57")
         {
-            Instance = this;
+            // Instance = this;
         }
         
-        public override string GetVersion() => "v0.0.1";
+        public override string GetVersion() => "0.0.1";
 
+        private SaveSettings saveSettings = new();
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
-            Log("Initializing");
+            Log("Initializing Precept 57");
 
             Instance = this;
 
             foreach (var precept in Precepts)
             {
-                precept.Hook();
-            }
+                var sprite = EmbeddedSprites.Get(precept.Sprite);
+                ItemHelper.AddNormalItem(sprite, "false", precept.Id, precept.Description);
 
-            Log("Initialized");
+                Func<SaveSettings, PreceptSettings> settings = precept.Settings;
+                BoolGetters[precept.Id] = _ => settings(saveSettings).Got;
+                BoolSetters[precept.Id] = value => settings(saveSettings).Got = value;
+
+                precept.Hook();
+                
+                var item = new BoolItem() {
+                    fieldName = precept.Id,
+                    setValue = false,
+                    name = precept.Id,
+                    UIDef = new MsgUIDef() { 
+                        name = new LanguageString("UI", precept.Id),
+                        shopDesc = new LanguageString("UI", precept.Description),
+                        sprite = new EmbeddedSprite() { key = precept.Sprite }
+                    }
+                };
+                Finder.DefineCustomItem(item);
+            }
+            ModHooks.GetPlayerBoolHook += GetPreceptBools;
+            ModHooks.SetPlayerBoolHook += SetPreceptBools;
+            ModHooks.SceneChanged += PlacePreceptsAtFixedPositions;
+
+            Log("Initialized Precept 57");
+        }
+        
+        private bool GetPreceptBools(string name, bool orig)
+        {
+            if (BoolGetters.TryGetValue(name, out var f))
+            {
+                return f(orig);
+            }
+            return orig;
+        }
+        
+        private bool SetPreceptBools(string name, bool orig)
+        {
+            if (BoolSetters.TryGetValue(name, out var f))
+            {
+                f(orig);
+            }
+            return orig;
+        }
+
+        private void PlacePreceptsAtFixedPositions(string s)
+        {
+            ItemChangerMod.CreateSettingsProfile(overwrite: false, createDefaultModules: false);
+
+            var placements = new List<AbstractPlacement>();
+            foreach (var precept in Precepts)
+            {
+                var name = precept.Id;
+                placements.Add(
+                    new CoordinateLocation() { x = precept.X, y = precept.Y, elevation = 10, sceneName = precept.Scene, name = name }
+                        .Wrap()
+                        .Add(Finder.GetItem(name)));
+            }
+            ItemChangerMod.AddPlacements(placements, conflictResolution: PlacementConflictResolution.Ignore);
+        }
+        
+        public void OnLoadLocal(SaveSettings s)
+        {
+            saveSettings = s;
+        }
+
+        public SaveSettings OnSaveLocal()
+        {
+            return saveSettings;
         }
     }
 }
