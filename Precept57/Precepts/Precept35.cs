e9c7fdcad7e30bb58a50e598cd8e4669cb6f53a3
HJain01 <jainh9999@gmail.com>
2022-05-30 21:39:05 -0400

    Add up and down text and change location

--- Precept57/Precepts/Precept35.cs
+++ Precept57/Precepts/Precept35.cs
@@ -12,10 +12,11 @@ namespace Precept57
 
         public override string Sprite => "Precept35.png";
         public override string Name => "Precept 35";
+        public override string Id => "Precept_35";
         public override string Description => "Add UP and DOWN text to the top and bottom of the screen.";
-        public override string Scene => "Crossroads_20"; //TODO: Figure out which scene to put it in
-        public override float X => 3.0f; //TODO: Figure out X coordinate in scene to put it at
-        public override float Y => 3.0f; //TODO: Figure out Y coordinate in scene to put it at
+        public override string Scene => "Tutorial_01"; //TODO: Figure out which scene to put it in
+        public override float X => 147.0f; //TODO: Figure out X coordinate in scene to put it at
+        public override float Y => 6.0f; //TODO: Figure out Y coordinate in scene to put it at
 
         public override PreceptSettings Settings(SaveSettings s) => s.Precept35;
 
@@ -31,15 +32,40 @@ namespace Precept57
 
         public void AddText()
         {
-            GameObject canvas = CanvasUtil.CreateCanvas(RenderMode.ScreenSpaceOverlay, new Vector2(2560, 1440));
+            if (HeroController.instance == null)
+            {
+                return;
+            }
+            
+            AddUpText();
+            AddDownText();
+        }
+
+        private static void AddUpText()
+        {
+            GameObject canvas = CanvasUtil.CreateCanvas(RenderMode.ScreenSpaceOverlay, new Vector2(Screen.width, Screen.height));
             Object.DontDestroyOnLoad(canvas);
-            var rectSizeDelta = new Vector2(600, 600);
-            var rectAnchorPosition = new Vector2(600, 600);
+            var rectSizeDelta = new Vector2(100, 100);
+            var rectAnchorPosition = new Vector2(0, Screen.height - 150);
             var rectMin = new Vector2(0.5f, 0f);
             var rectMax = new Vector2(0.5f, 0f);
             var rectPivot = new Vector2(0.5f, 0.5f);
             var rectData = new CanvasUtil.RectData(rectSizeDelta, rectAnchorPosition, rectMin, rectMax, rectPivot);
-            CanvasUtil.CreateTextPanel(canvas, "UP", 48, TextAnchor.UpperCenter, rectData, true)
+            CanvasUtil.CreateTextPanel(canvas, "UP", 48, TextAnchor.UpperCenter, rectData)
+                .GetComponent<Text>();
+        }
+        
+        private static void AddDownText()
+        {
+            GameObject canvas = CanvasUtil.CreateCanvas(RenderMode.ScreenSpaceOverlay, new Vector2(2560, 1440));
+            Object.DontDestroyOnLoad(canvas);
+            var rectSizeDelta = new Vector2(200, 200);
+            var rectAnchorPosition = new Vector2(0, 150);
+            var rectMin = new Vector2(0.5f, 0f);
+            var rectMax = new Vector2(0.5f, 0f);
+            var rectPivot = new Vector2(0.5f, 0.5f);
+            var rectData = new CanvasUtil.RectData(rectSizeDelta, rectAnchorPosition, rectMin, rectMax, rectPivot);
+            CanvasUtil.CreateTextPanel(canvas, "DOWN", 48, TextAnchor.LowerCenter, rectData, true)
                 .GetComponent<Text>();
         }
     }
