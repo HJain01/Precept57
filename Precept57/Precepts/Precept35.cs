1593a8315e674856082074b748d86aaa57afe041
HJain01 <jainh9999@gmail.com>
2022-06-04 13:24:47 -0400

    putting the charm back in crossroads

--- Precept57/Precepts/Precept35.cs
+++ Precept57/Precepts/Precept35.cs
@@ -19,9 +19,9 @@ namespace Precept57
         public override string DescOne => "Up is Up, Down is Down";
 
         public override string DescTwo => "UP and DOWN will be on the top and bottom of your screen so you can never forget";
-        public override string Scene => "Tutorial_01";
-        public override float X => 45.5f;
-        public override float Y => 11.4f;
+        public override string Scene => "Crossroads_36";
+        public override float X => 37.5f;
+        public override float Y => 29.1f;
 
         public override PreceptSettings Settings(SaveSettings s) => s.Precept35;
 
